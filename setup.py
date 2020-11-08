785e9293cd59d624d3ea921e96653bb450734199
jarbasal <jarbasai@mailfence.com>
2020-11-08 20:33:49 +0000

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='HiveMind-PtT',
-    version='0.1.0.',
+    version='0.1.0',
     packages=['mycroft_ptt', 'mycroft_ptt.speech',
               'mycroft_ptt.res',
               'mycroft_ptt.configuration'],
