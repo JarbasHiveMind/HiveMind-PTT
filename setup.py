c20fd5be6327f61d460999b4e0738751dae405f4
jarbasal <jarbasai@mailfence.com>
2020-12-04 17:03:29 +0000

    fix manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='HiveMind-PtT',
-    version='0.1.4',
+    version='0.1.6',
     packages=['mycroft_ptt', 'mycroft_ptt.speech',
               'mycroft_ptt.res',
               'mycroft_ptt.configuration'],
